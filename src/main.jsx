fafd755227be5a297c812db770c0f0ff3384761b
adradivya001 <adradivya001@gmail.com>
2026-09-24 12:23:05 +0530

    Ensure Teja light theme styles are imported in main entrypoint

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
+import './colleges/sri-gitam/theme.css';
+import './colleges/teja/theme.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
